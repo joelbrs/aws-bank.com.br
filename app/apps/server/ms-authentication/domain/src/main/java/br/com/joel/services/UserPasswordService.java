dbad0febd1d5b599fd1cdf8e3fb47d8e72ba482a
Joel da Silva Pereira Filho <joelfilho02812@gmail.com>
2025-09-09 13:26:27 -0300

    feat(ms-authentication): add user's passwords service

---
+++ app/apps/server/ms-authentication/domain/src/main/java/br/com/joel/services/UserPasswordService.java
@@ -0,0 +1,32 @@
+package br.com.joel.services;
+
+import br.com.joel.domain.domain.UserPassword;
+import br.com.joel.ports.database.UserPasswordRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class UserPasswordService {
+
+    private final UserPasswordRepository userPasswordRepository;
+    private final CryptoService cryptoService;
+
+    public void create(UserPassword userPassword) {
+        this.validate(userPassword);
+
+        userPassword.setLoginPassword(cryptoService.hash(userPassword.getLoginPassword()));
+        userPassword.setActionsPassword(cryptoService.hash(userPassword.getActionsPassword()));
+
+        userPasswordRepository.create(userPassword);
+    }
+
+    private void validate(UserPassword userPassword) {
+        if (userPassword.getActionsPassword().length() != 6) {
+            throw new IllegalArgumentException("Invalid actions password");
+        }
+
+        if (userPassword.getLoginPassword().length() != 8) {
+            throw new IllegalArgumentException("Invalid login password");
+        }
+    }
+}
+
