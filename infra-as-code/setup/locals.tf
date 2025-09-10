f976a4e9744cb4a4b7696df9e139bf2fa73558e1
Joel da Silva Pereira Filho <joelfilho02812@gmail.com>
2025-09-09 22:43:06 -0300

    fix(terraform): image tag

--- infra-as-code/setup/locals.tf
+++ infra-as-code/setup/locals.tf
@@ -1,6 +1,6 @@
 locals {
   azs  = ["${var.aws_region}a", "${var.aws_region}b", "${var.aws_region}c"]
 
-  ms_authentication_image_tag = "${var.environment}-${var.ms_authentication_project_name}-${var.apps_version_default}"
+  ms_authentication_image_tag = "${var.environment}-${var.ms_authentication_project_name}:${var.apps_version_default}"
   ms_authentication_container_name = "${var.environment}-${var.ms_authentication_project_name}"
 }
