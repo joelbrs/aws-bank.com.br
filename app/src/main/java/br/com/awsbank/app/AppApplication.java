65ddb7b6af1beea74d218c5c74de7e58573b046c
Joel da Silva Pereira Filho <joelfilho02812@gmail.com>
2026-05-03 17:29:03 -0300

    chore(app): init project

---
+++ app/src/main/java/br/com/awsbank/app/AppApplication.java
@@ -0,0 +1,15 @@
+package br.com.awsbank.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class AppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
+}
